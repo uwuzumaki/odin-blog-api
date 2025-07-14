f07b492a06114bf657ed6932103a281f9294f538
Kris Wong <krischwong@gmail.com>
2025-07-14 00:10:18 -0400

    minor stuff

--- routers/commentRouter.js
+++ routers/commentRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
   return res.send("comment router");
 });
 
--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,9 @@
 import post from "./postRouter.js";
 import comment from "./commentRouter.js";
+import auth from "./authRouter.js";
 
 export default {
   post,
   comment,
+  auth,
 };
