b36304270f2c7289b02857e0896d7b17200e07e6
Kris Wong <krischwong@gmail.com>
2025-07-14 00:11:32 -0400

    Added route protection

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import authentication from "../authentication/jwt.js";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.send("post router");
+router.post("/", authentication.getToken, (req, res) => {
+  jwt.verify(req.token, process.env.JWT_SECRET, (err, data) => {
+    if (err) {
+      res.json({ err });
+    } else {
+      res.json({ data });
+    }
+  });
 });
 
 export default router;
