e948915fd894ff8392cc8795d32874307eda9fbf
Kris Wong <krischwong@gmail.com>
2025-07-14 00:08:18 -0400

    Added login and register endpoints(only used for creating admin)

---
+++ controller/auth.js
@@ -0,0 +1,32 @@
+import jwt from "../authentication/jwt.js";
+// import bcrypt from "bcryptjs";
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+const loginPost = async (req, res) => {
+  const token = await jwt.sign(req.body.username, req.body.password);
+  res.json({
+    token,
+  });
+};
+
+// One time registration for Admin user
+// const registerPost = async (req, res) => {
+//   console.log(req.body.username);
+//   const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//   const user = await prisma.admin.create({
+//     data: {
+//       username: req.body.username,
+//       password: hashedPassword,
+//     },
+//   });
+//   res.json({
+//     response: user,
+//   });
+// };
+
+export default {
+  loginPost,
+  //   registerPost,
+};
