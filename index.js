00adcc61944eee60fed74d30351f01ee4a5a8497
Kris Wong <krischwong@gmail.com>
2025-07-16 00:15:57 -0400

    added passport

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import router from "./routers/index.js";
+import passport from "passport";
 import "dotenv/config";
 
 // App instantiation
 const app = express();
+
+import "./authentication/passport.js";
+
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
 
 // Routes
 app.use("/post", router.post);
