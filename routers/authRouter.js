7c26820f2a564871e69af02637dd799298279f59
Kris Wong <krischwong@gmail.com>
2025-07-16 00:16:37 -0400

    change route to authenticate with a local passport strategy

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,9 +1,14 @@
+import passport from "passport";
 import { Router } from "express";
 import authController from "../controller/auth.js";
 
 const router = Router();
 
-router.post("/", authController.loginPost);
+router.post(
+  "/",
+  passport.authenticate("local", { session: false }),
+  authController.loginPost
+);
 // router.post("/register", authController.registerPost);
 
 export default router;
