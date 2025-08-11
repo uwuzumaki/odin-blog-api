9bbe98c29ade1148840983cf08ff63baae355802
Kris Wong <krischwong@gmail.com>
2025-08-11 01:05:27 -0400

    Updated cors settings

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ import "./authentication/passport.js";
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(passport.initialize());
 
 // Routes
