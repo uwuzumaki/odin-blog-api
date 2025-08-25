8eb595fa5c9b6d49e661febf9ce09d2ac8b00b9e
Kris Wong <krischwong@gmail.com>
2025-08-24 22:16:23 -0400

    Added routes for verify and logout'

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -5,9 +5,21 @@ import authController from "../controller/auth.js";
 const router = Router();
 
 router.post(
-  "/",
+  "/login",
   passport.authenticate("local", { session: false }),
   authController.loginPost
+);
+
+router.get(
+  "/verify",
+  passport.authenticate("jwt", { session: false }),
+  authController.verifyGet
+);
+
+router.get(
+  "/logout",
+  passport.authenticate("jwt", { session: false }),
+  authController.logoutGet
 );
 // router.post("/register", authController.registerPost);
 
