7061c50c7b079b79b9e1db23a2c9b45df2b3a684
Kris Wong <krischwong@gmail.com>
2025-08-27 22:04:43 -0400

    Allowed another localhost in cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
