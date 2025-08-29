2b3e05d6b9faad9570b48dc5f8498c4f949e7f28
Kris Wong <krischwong@gmail.com>
2025-08-28 23:36:34 -0400

    removed registration route

--- controller/auth.js
+++ controller/auth.js
@@ -1,15 +1,11 @@
 import jwt from "../authentication/jwt.js";
-import bcrypt from "bcryptjs";
-import { PrismaClient } from "@prisma/client";
+// import bcrypt from "bcryptjs";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 const loginPost = async (req, res) => {
-  console.log("got here");
   const token = await jwt.sign(req.body.username, req.body.password);
-  console.log(req.body.username, req.body.password);
-  console.log(req.body);
-  console.log(token);
   res.cookie("token", token, {
     httpOnly: true,
     sameSite: "strict",
@@ -34,23 +30,23 @@ const logoutGet = async (req, res) => {
   }
 };
 // One time registration for Admin user
-const registerPost = async (req, res) => {
-  console.log(req.body.username);
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const user = await prisma.admin.create({
-    data: {
-      username: req.body.username,
-      password: hashedPassword,
-    },
-  });
-  res.json({
-    response: user,
-  });
-};
+// const registerPost = async (req, res) => {
+//   console.log(req.body.username);
+//   const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//   const user = await prisma.admin.create({
+//     data: {
+//       username: req.body.username,
+//       password: hashedPassword,
+//     },
+//   });
+//   res.json({
+//     response: user,
+//   });
+// };
 
 export default {
   loginPost,
   verifyGet,
   logoutGet,
-  registerPost,
+  // registerPost,
 };
