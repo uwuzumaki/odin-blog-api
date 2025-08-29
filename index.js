3f8deedb96e5197f6c2ecfe44c7fa7feeedf79c7
Kris Wong <krischwong@gmail.com>
2025-08-28 22:58:49 -0400

    Added admin deploy to cors

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://elaborate-sherbet-930a23.netlify.app",
+      "https://leafy-begonia-09ba39.netlify.app",
     ],
     credentials: true,
   })
